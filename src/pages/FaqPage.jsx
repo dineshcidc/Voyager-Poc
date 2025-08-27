434c5db2b6d9ac5dad6061c710f6f9a1fbc0d35f
GandhinathanSubramanian <gandhinathan.subramanian@concertidc.com>
2025-08-28 00:49:45 +0530

    feat: implement FAQ section with collapsible answers and integrate into HomePage and FaqPage

--- src/pages/FaqPage.jsx
+++ src/pages/FaqPage.jsx
@@ -1,49 +1,9 @@
-import { useState } from "react";
-import { FaPlus, FaMinus } from "react-icons/fa";
 import { BiSearch } from "react-icons/bi";
+
+import FaqContent from "../components/FaqContent";
 import ApplyNowOption from "../components/ApplyNowOption";
 
-const faqData = [
-  {
-    question:
-      "Learn more about our Wizz Voyager Card with these frequently asked questions.",
-    answer:
-      "It offers multi-currency support, low forex markup, global acceptance, and 24x7 customer support.",
-  },
-  {
-    question: "What is the Wizz Voyager Forex Card?",
-    answer: `The Wizz Voyager Forex Card is a secure, RBI-compliant, multi-currency prepaid travel card issued by Unimoni. It allows you to:
-    Spend globally at ATMs, POS terminals, and international e-commerce platforms
-    Load and manage 17 foreign currencies seamlessly via the Wizz Voyager App
-    Access India's only 24x7 live customer support on WhatsApp - via both call & Chat
-    It's the peect travel companion for Indian students, tourists, and professionals going abroad.`,
-  },
-  {
-    question:
-      "Who is eligible to apply for the Wizz Voyager Forex Card in India?",
-    answer:
-      "Any Indian resident with valid KYC documents is eligible to apply for the Wizz Voyager Forex Card.",
-  },
-  {
-    question:
-      "What makes Wizz Voyager the best Forex Card for Indian travelers?",
-    answer:
-      "It offers multi-currency support, low forex markup, global acceptance, and 24x7 customer support.",
-  },
-  {
-    question: "How can exchange currencies from the Wizz Voyager mobile app?",
-    answer:
-      "You can load, reload, and exchange currencies easily via the Wizz Voyager mobile app.",
-  },
-];
-
 const FaqPage = () => {
-  const [openIndex, setOpenIndex] = useState(null);
-
-  const toggleFaq = (index) => {
-    setOpenIndex(openIndex === index ? null : index);
-  };
-
   return (
     <div className="flex flex-col gap-10 md:gap-25">
       {/* banner section */}
@@ -91,35 +51,7 @@ const FaqPage = () => {
       </div>
 
       {/* FAQ content */}
-      <div className="flex flex-col gap-6">
-        {faqData.map((item, index) => (
-          <div key={index} className="bg-gray-100 rounded-2xl p-5">
-            <div
-              className="flex items-center justify-between cursor-pointer"
-              onClick={() => toggleFaq(index)}
-            >
-              <h1
-                className={`${
-                  openIndex === index ? "text-gradient-1-end" : ""
-                } text-lg font-medium`}
-              >
-                {item.question}
-              </h1>
-              {openIndex === index ? (
-                <FaMinus className="text-gray-600" />
-              ) : (
-                <FaPlus className="text-gray-600" />
-              )}
-            </div>
-
-            {openIndex === index && (
-              <div className="mt-3 text-gray-700 text-sm md:text-base whitespace-pre-line">
-                {item.answer}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
+      <FaqContent />
 
       <ApplyNowOption />
     </div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,20 @@
+import FaqContent from "../components/FaqContent";
+
 const HomePage = () => {
   return (
     <div className="text-success-gradient-start z-10 text-black">
-      Inprogress
+      <div className="flex flex-col items-center gap-10">
+        <div>
+          <h1 className="text-2xl md:text-5xl font-bold capitalize text-center">
+            frequently asked questions
+          </h1>
+          <h2 className="text-xs md:text-xl mt-3 text-center">
+            Learn more about how the Wizz Voyager Card works
+          </h2>
+        </div>
+
+        <FaqContent />
+      </div>
     </div>
   );
 };
