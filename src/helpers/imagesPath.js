146cd5d7219e79e6e54a275c6eeb53f878915a30
GandhinathanSubramanian <gandhinathan.subramanian@concertidc.com>
2025-08-28 01:19:17 +0530

    feat: Enhance Homepage layout and add new images
    - Updated imagesPath.js to include new images for Visa card and Unimoni Wizz Group.
    - Redesigned Homepage component to include an About Us section, Stats section, and Offer section with enhanced layout.
    - Integrated new images into the Offer section and added a new Apply Now section.

--- src/helpers/imagesPath.js
+++ src/helpers/imagesPath.js
@@ -2,16 +2,18 @@ import img404 from "../assets/404.png";
 import homeBg1 from "../assets/bg_1.svg";
 import homeBg2 from "../assets/bg_2.svg";
 import homeBg3 from "../assets/bg_3.svg";
-import homeOfferImg1 from "../assets/offer_page_carousel1.svg";
-import blogDetailsBanner from "../assets/blog_details_banner.png";
 import visaCardImg from "../assets/visa_card.png";
+import homeOfferImg1 from "../assets/offer_page_carousel1.svg";
+import unimoniWizzGroup from "../assets/unimoni_wizz_group.svg";
+import blogDetailsBanner from "../assets/blog_details_banner.png";
 
 export const ImagesPath = {
   BG_IMAGE_1: homeBg1,
   BG_IMAGE_2: homeBg2,
   BG_IMAGE_3: homeBg3,
-  OFFER_IMG_1: homeOfferImg1,
-  PAGE_NOT_FOUND: img404,
-  BLOG_BANNER: blogDetailsBanner,
   VISA_CARD: visaCardImg,
+  PAGE_NOT_FOUND: img404,
+  OFFER_IMG_1: homeOfferImg1,
+  BLOG_BANNER: blogDetailsBanner,
+  UNIMONI_WIZZ_GROUP: unimoniWizzGroup,
 };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,137 @@
 import FaqContent from "../components/FaqContent";
+import { ImagesPath } from "../helpers/imagesPath";
+import ApplyNowOption from "../components/ApplyNowOption";
 
 const HomePage = () => {
   return (
-    <div className="text-success-gradient-start z-10 text-black">
+    <div className="flex flex-col gap-20">
+      {/* About Us */}
+      <div className="flex flex-col gap-5 md:gap-12">
+        <div className="flex flex-col items-center gap-2 md:gap-4">
+          <h1 className="uppercase text-quaternary-1 text-base">about</h1>
+          <h1 className="text-xl md:text-4xl font-bold">
+            Innovation you can Trust
+          </h1>
+        </div>
+        {/*  */}
+        {/* Stats Section */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-16 items-center text-center">
+          <div className="flex flex-col items-center">
+            <h2 className="text-3xl md:text-5xl">25+</h2>
+            <p className="text-sm md:text-lg text-gray-600">
+              Years of Expertise
+            </p>
+          </div>
+          <div className="flex flex-col items-center">
+            <h2 className="text-3xl md:text-5xl">5mn</h2>
+            <p className="text-sm md:text-lg text-gray-600">Happy Customers</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <h2 className="text-3xl md:text-5xl">350+</h2>
+            <p className="text-sm md:text-lg text-gray-600">Branch Network</p>
+          </div>
+        </div>
+
+        {/* Divider */}
+        <div className="w-full border-t border-gray-300 max-w-4xl"></div>
+
+        {/* Logo Section */}
+        <div className="flex justify-center">
+          <img
+            src={ImagesPath.UNIMONI_WIZZ_GROUP}
+            alt="Unimoni Logo"
+            className="object-contain h-13 md:h-27"
+          />
+        </div>
+      </div>
+
+      {/* Offer Section */}
+      <div className="flex flex-col gap-5 md:gap-12">
+        <div className="flex flex-col items-center gap-2 md:gap-4">
+          <h1 className="uppercase text-quaternary-1 text-base">Offers</h1>
+          <h1 className="text-xl md:text-4xl font-bold">
+            Rewarding the Way You Move
+          </h1>
+          <h1 className="text-xs md:text-lg">
+            You're always on - so are we. Enjoy elite travel perks,
+            international deals, and experiences tailored for the modern nomad.
+          </h1>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-x-6.5 gap-y-10 px-4">
+          <div className="relative rounded-3xl overflow-hidden">
+            {/* Background Image */}
+            <img
+              src={ImagesPath.VISA_CARD}
+              alt="visa_card"
+              className="w-full h-85 md:h-101 object-cover"
+            />
+
+            {/* Glassmorphism Overlay */}
+            <div className="absolute bottom-0 w-full p-5 bg-white/20 backdrop-blur-md flex justify-center items-center gap-6">
+              <div className="flex flex-col gap-2">
+                <h3 className="text-xl font-bold text-white">
+                  banner title HC
+                </h3>
+                <p className="text-xs text-white/90">thumbnail title HC</p>
+              </div>
+              <div>
+                <button className="text-xs md:text-sm bg-gradient-3 rounded-xl p-2 font-bold text-white w-26 hover:cursor-pointer">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="relative rounded-3xl overflow-hidden">
+            {/* Background Image */}
+            <img
+              src={ImagesPath.VISA_CARD}
+              alt="visa_card"
+              className="w-full h-85 md:h-101 object-cover"
+            />
+
+            {/* Glassmorphism Overlay */}
+            <div className="absolute bottom-0 w-full p-5 bg-white/20 backdrop-blur-md flex justify-center items-center gap-6">
+              <div className="flex flex-col gap-2">
+                <h3 className="text-xl font-bold text-white">
+                  banner title HC
+                </h3>
+                <p className="text-xs text-white/90">thumbnail title HC</p>
+              </div>
+              <div>
+                <button className="text-xs md:text-sm bg-gradient-3 rounded-xl p-2 font-bold text-white w-26 hover:cursor-pointer">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="relative rounded-3xl overflow-hidden">
+            {/* Background Image */}
+            <img
+              src={ImagesPath.VISA_CARD}
+              alt="visa_card"
+              className="w-full h-85 md:h-101 object-cover"
+            />
+
+            {/* Glassmorphism Overlay */}
+            <div className="absolute bottom-0 w-full p-5 bg-white/20 backdrop-blur-md flex justify-center items-center gap-6">
+              <div className="flex flex-col gap-2">
+                <h3 className="text-xl font-bold text-white">
+                  banner title HC
+                </h3>
+                <p className="text-xs text-white/90">thumbnail title HC</p>
+              </div>
+              <div>
+                <button className="text-xs md:text-sm bg-gradient-3 rounded-xl p-2 font-bold text-white w-26 hover:cursor-pointer">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* FAQ Section */}
       <div className="flex flex-col items-center gap-10">
         <div>
           <h1 className="text-2xl md:text-5xl font-bold capitalize text-center">
@@ -15,6 +144,9 @@ const HomePage = () => {
 
         <FaqContent />
       </div>
+
+      {/* apply now section */}
+      <ApplyNowOption />
     </div>
   );
 };
